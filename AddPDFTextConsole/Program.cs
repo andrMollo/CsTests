2aa2f36b3aa3a167c932b2e144e8a60fe28fffd5
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2024-01-08 16:45:56 +0100

    Add path for base file and watermark

--- AddPDFTextConsole/Program.cs
+++ AddPDFTextConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace AddPDFTextConsole
 {
     internal class Program
     {
+        private static readonly string baseFile = @"C:\Users\AndreaMollo\Desktop\testPDF\origin.pdf";
+
+        private static readonly string watermarkFile = @"C:\Users\AndreaMollo\Desktop\testPDF\watermark.jpg";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+
             Console.ReadLine();
         }
     }
